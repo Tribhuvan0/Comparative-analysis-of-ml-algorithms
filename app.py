78f2b90651a6a77e0df9b125adc1b7b53e325937
Tribhuvan Mishra <74012568+Tribhuvan0@users.noreply.github.com>
2022-12-01 22:08:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pickle
+import sklearn
+import pandas as pd
+import numpy as np
+from PIL import Image
+model = pickle.load(open('model1.sav', 'rb'))
+
+st.title('House price Prediction')
+st.sidebar.header('House Data')
+image = Image.open('bb.jpg')
+st.image(image, '')
+
+# FUNCTION
+def user_report():
+  House_Age = st.sidebar.slider('House Age', 0,45, 0 )
+  Nearest_Metro = st.sidebar.slider('Nearest Metro (km)', 20,1000, 10 )
+  Convenience_stores = st.sidebar.slider('Convenience stores', 0,10, 1 )
+  Latitude = st.sidebar.slider('Latitude', 24.0,25.0, 24.0 )
+  Longitude = st.sidebar.slider('longitude', 121.4735,121.56627, 121.4730)
+
+  user_report_data = {
+      'House Age':House_Age,
+      'Distance from nearest Metro station (km)':Nearest_Metro,
+      'Number of convenience stores':Convenience_stores,
+      'latitude':Latitude,
+      'longitude':Longitude,
+  }
+  report_data = pd.DataFrame(user_report_data, index=[0])
+  return report_data
+
+user_data = user_report()
+st.header('House Data')
+st.write(user_data)
+
+Price = model.predict(user_data)
+st.subheader('House Price Per Unit Area')
+st.subheader('$'+str(np.round(Price[0], 2)))
